17db50bf4231fa7fe5603ad8ec409285d461cc97
rcsCrew <51542841+rcsCrew@users.noreply.github.com>
2023-04-07 07:22:25 +0000

    animar ok

--- src/scripts/nav-bar.js
+++ src/scripts/nav-bar.js
@@ -13,15 +13,29 @@ window.addEventListener('scroll', () => {
 
 //
 
-window.addEventListener('scroll', function() {
-  const elementos = document.querySelectorAll('.animar');
-  const altura = window.innerHeight;
-  const punto = 250;
+// window.addEventListener('scroll', function() {
+//   const elementos = document.querySelectorAll('.animar');
+//   const altura = window.innerHeight;
+//   const punto = 250;
 
-  elementos.forEach(elemento => {
-    const distancia = elemento.getBoundingClientRect().top;
-    if (distancia < altura - punto) {
-      elemento.classList.add('visible');
-    }
-  });
-});
+//   elementos.forEach(elemento => {
+//     const distancia = elemento.getBoundingClientRect().top;
+//     if (distancia < altura - punto) {
+//       elemento.classList.add('visible');
+//     }
+//   });
+// });
+
+
+const scroll = document.querySelector('ativar');
+
+window.addEventListener('scroll', () => {
+  if( window.scrollY > 200) {
+    scroll.classList.remove('animar0')
+    scroll.classList.add('animar1');
+  }
+  else{
+    scroll.classList.remove('animar1')
+    scroll.classList.add('animar0');
+  }
+})
