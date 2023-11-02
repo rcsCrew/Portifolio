f26f2b043b3f0f1983fcd4308fae6a204e11546b
rcsCrew <51542841+rcsCrew@users.noreply.github.com>
2023-11-01 23:29:10 -0300

    85%

--- src/scripts/nav-bar.js
+++ src/scripts/nav-bar.js
@@ -30,7 +30,7 @@ window.addEventListener('scroll', () => {
 const scroll = document.querySelector('ativar');
 
 window.addEventListener('scroll', () => {
-  if( window.scrollY > 200) {
+  if( window.scrollY > 70) {
     scroll.classList.remove('animar0')
     scroll.classList.add('animar1');
   }
@@ -38,5 +38,19 @@ window.addEventListener('scroll', () => {
     scroll.classList.remove('animar1')
     scroll.classList.add('animar0');
   }
-})
+});
+
+const scroll2 = document.querySelector('ativar2');
+
+window.addEventListener('scroll', () => {
+  if( window.scrollY > 600) {
+    scroll2.classList.remove('animar0')
+    scroll2.classList.add('animar1');
+  }
+  else{
+    scroll2.classList.remove('animar1')
+    scroll2.classList.add('animar0');
+  }
+});
+
 
